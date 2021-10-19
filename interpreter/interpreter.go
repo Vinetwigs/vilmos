7d40158f9cbab20c9a719c7b35dcd3d10c99fdac
Vinetwigs <espositovincenzopio@gmail.com>
2021-10-19 19:13:52 +0200

    - added loading and getter for the image

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,1 +1,36 @@
 package interpreter
+
+import (
+	"image"
+	"os"
+)
+
+type Interpreter struct {
+	image image.Image
+}
+
+func NewInterpreter() *Interpreter {
+	interpreter := new(Interpreter)
+	interpreter.image = nil
+	return interpreter
+}
+
+func (i *Interpreter) LoadImage(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	image, _, err := image.Decode(f)
+
+	if err != nil {
+		return err
+	} else {
+		i.image = image
+		return nil
+	}
+}
+
+func (i *Interpreter) GetImage() image.Image {
+	return i.image
+}
