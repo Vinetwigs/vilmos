9cbb2a011dc77aefb2c7209561c4b35acaf0a6fd
Vinetwigs <espositovincenzopio@gmail.com>
2021-10-19 20:45:51 +0200

    - implemetented Equals() method

--- pixel/pixel.go
+++ pixel/pixel.go
@@ -12,3 +12,7 @@ type Pixel struct {
 func (p *Pixel) String() string {
 	return "{R: " + strconv.Itoa(p.R) + " G: " + strconv.Itoa(p.G) + " B: " + strconv.Itoa(p.B) + " A: " + strconv.Itoa(p.A) + "}"
 }
+
+func (p *Pixel) Equals(other Pixel) bool {
+	return (p.R == other.R) && (p.G == other.G) && (p.B == other.B) && (p.A == other.A)
+}
