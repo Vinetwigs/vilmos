fc7dc748664927cc642f16cba1d28df4903b423e
Vinetwigs <espositovincenzopio@gmail.com>
2021-10-22 13:45:30 +0200

    - removed alpha channel from pixel struct implementation

--- pixel/pixel.go
+++ pixel/pixel.go
@@ -6,13 +6,12 @@ type Pixel struct {
 	R int
 	G int
 	B int
-	A int
 }
 
 func (p *Pixel) String() string {
-	return "{R: " + strconv.Itoa(p.R) + " G: " + strconv.Itoa(p.G) + " B: " + strconv.Itoa(p.B) + " A: " + strconv.Itoa(p.A) + "}"
+	return "{R: " + strconv.Itoa(p.R) + " G: " + strconv.Itoa(p.G) + " B: " + strconv.Itoa(p.B) + "}"
 }
 
 func (p *Pixel) Equals(other Pixel) bool {
-	return (p.R == other.R) && (p.G == other.G) && (p.B == other.B) && (p.A == other.A)
+	return (p.R == other.R) && (p.G == other.G) && (p.B == other.B)
 }
